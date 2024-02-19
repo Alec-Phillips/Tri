f1055c21adf0978edd83e7463220d20a0681045f
Alec-Phillips <70179843+Alec-Phillips@users.noreply.github.com>
2024-02-18 23:21:02 -0500

    basic operators implementation

--- tri.js
+++ tri.js
@@ -1,4 +1,8 @@
 let mode = "base-3"
+let currentOperand = ""
+let currentOperatorFn = null
+let currentOperatorButton = null
+let operatorJustActivated = false
 
 const CONFIGS = {
   "base-3": {
@@ -11,6 +15,13 @@ const CONFIGS = {
       8, 42, 43, 45, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 61
     ],
   }
+}
+
+const operators = {
+  "+": (operand1, operand2) => Number(operand1) + Number(operand2),
+  "-": (operand1, operand2) => Number(operand1) - Number(operand2),
+  "*": (operand1, operand2) => Number(operand1) * Number(operand2),
+  "/": (operand1, operand2) => Number(operand1) / Number(operand2),
 }
 
 const getAllowedKeys = () => CONFIGS[mode].allowedKeys
@@ -41,15 +52,42 @@ const updateButtons = (newMode) => {
   }
 }
 
-const buttons = document.querySelectorAll(".calc-button")
+const buttons = document.querySelectorAll(".inputtable")
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (screenInput.value === "0") {
+    if (currentOperatorFn && operatorJustActivated) {
+      currentOperatorButton.removeAttribute?.("style")
       screenInput.value = button.innerHTML
+      operatorJustActivated = false
     } else {
-      screenInput.value += button.innerHTML
+      if (screenInput.value === "0") {
+        screenInput.value = button.innerHTML
+      } else {
+        screenInput.value += button.innerHTML
+      }
     }
   })
+})
+
+const operatorButtons = document.querySelectorAll(".operator")
+operatorButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener("click", () => {
+    currentOperatorButton?.removeAttribute?.("style")
+    currentOperatorButton = operatorButton
+    currentOperatorButton.style.backgroundColor = "hsl(66, 42%, 75%)"
+    currentOperand = screenInput.value
+    currentOperatorFn = operators[operatorButton.innerHTML]
+    operatorJustActivated = true
+  })
+})
+
+const equalsButton = document.getElementById("61")
+equalsButton.addEventListener("click", () => {
+  const operand2 = screenInput.value
+  const result = currentOperatorFn(currentOperand, operand2)
+  screenInput.value = result
+  currentOperand = result
+  currentOperatorButton?.removeAttribute?.("style")
 })
 
 window.onload = () => {
