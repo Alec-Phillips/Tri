3c5ca315f92d747e1a18c392a2494984f3236726
Alec-Phillips <70179843+Alec-Phillips@users.noreply.github.com>
2024-02-18 19:23:41 -0500

    remove unused code

--- tri.js
+++ tri.js
@@ -1,27 +1,19 @@
 let mode = "base-3"
 
-
 const CONFIGS = {
   "base-3": {
     allowedKeys: [
       42, 43, 45, 47, 48, 49, 50,
     ],
-    buttons: [
-      "0", "1", "2", "+", "-", "*", "/",
-    ]
   },
   "base-10": {
     allowedKeys: [
       42, 43, 45, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
     ],
-    buttons: [
-      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/",
-    ]
   }
 }
 
 const getAllowedKeys = () => CONFIGS[mode].allowedKeys
-const getButtons = () => CONFIGS[mode].buttons
 
 const base10Buttons = document.querySelectorAll(".base-10-button")
 
@@ -62,7 +54,6 @@ buttons.forEach((button) => {
     screenInput.value += button.innerHTML
   })
 })
-
 
 
 window.onload = () => {
